2d197e9181dcb7c17fe0063407f8c84e776be6f1
Chrito196 <Christopher-Braun@outlook.com>
2025-11-08 12:32:01 +0100

    New Code > Code optimate and simple

--- script.js
+++ script.js
@@ -1,70 +1,59 @@
-
 // Meine Bilder in klein
-const container = document.getElementById ("holidaypicture");
+const container = document.getElementById("holidaypicture");
+const dialog = document.getElementById("pictureDialog");
+const bigImg = document.getElementById("bigImg");
 
-//Meine Bilder 
-let pictures = [ //Bilder NAMEN + GRÖßE muss noch angepasst werden
-    "./img/IMG_5554.jpg",
-    "./img/IMG_5555.jpg",
-    "./img/IMG_5607.jpg",
-    "./img/IMG_5663.jpg",
-    "./img/IMG_5726.jpg",
-    "./img/Strand1.jpg",
-    "./img/IMG_5759.jpg",
-    "./img/IMG_5765.jpg",
-    "./img/IMG_5771.jpg",
-    "./img/IMG_9154.jpg",    
+
+//Meine Bilder
+const pictures = [
+  //Bilder NAMEN + GRÖßE muss noch angepasst werden
+  "./img/IMG_5554.jpg",
+  "./img/IMG_5555.jpg",
+  "./img/IMG_5607.jpg",
+  "./img/IMG_5663.jpg",
+  "./img/IMG_5726.jpg",
+  "./img/Strand1.jpg",
+  "./img/IMG_5759.jpg",
+  "./img/IMG_5765.jpg",
+  "./img/IMG_5771.jpg",
+  "./img/IMG_9154.jpg",
 ];
 
 // Bildebeschreibung
 const pictureDescription = [
-    "Hotel Außenansicht", 
-    "Strand auf Mallorca",
-    "Blick vom Hotelzimmer Morgens",
-    "Blick vom Hotelzimmer Mittags",
-    "Aquarium mit Meerfischen",
-    "Strand auf Mallorca",
-    "Schildkröten von Oben",
-    "Hai im Aquarium",
-    "Hai im Aquarium aus der Nähe",
-    "Strand Spaziergang auf Mallorca",  
-  ];
-
-// Overlay-Elemente
-const overlay = document.getElementById("overlay");
-const overlayImg = document.getElementById("overlay-img");
-const Description = document.getElementById ("pictureDescription")
-const closeBtn = document.getElementById("close-btn");
-const prevBtn = document.getElementById("prev-btn"); // noch hinzufügen 
-const nextBtn = document.getElementById("next-btn"); // noch hinzufügen
-
+  "Hotel Außenansicht",
+  "Strand auf Mallorca",
+  "Blick vom Hotelzimmer Morgens",
+  "Blick vom Hotelzimmer Mittags",
+  "Aquarium mit Meerfischen",
+  "Strand auf Mallorca",
+  "Schildkröten von Oben",
+  "Hai im Aquarium",
+  "Hai im Aquarium aus der Nähe",
+  "Strand Spaziergang auf Mallorca",
+];
 
 //Eine Schleife, um jedes Bild einzufügen    KLEINE BILDER
 for (let i = 0; i < pictures.length; i++) {
-  const img = document.createElement("img");    // Richtiges <img>-Element erstellen 
-  img.src = pictures[i];                        // Pfad zum Bild setzen
-  img.style.width = "250px";                    // Kleine Anzeigegröße
-  img.style.margin = "10px";                    // Abstand zwischen Bildern
-  img.style.cursor = "pointer";                 // Zeigt, dass man klicken kann
-  img.style.borderRadius = "10px";
-  
-
+  const img = document.createElement("img"); // Richtiges <img>-Element erstellen
+  img.src = pictures[i]; // Pfad zum Bild setzen
+  img.alt = pictureDescription[i] || `Bild ${i + 1}`;
+  img.classList.add("small-img"); // img css style
 
   // Wenn auf das Bild geklickt wird
-  img.addEventListener("click", function() {
-    overlayImg.src = this.src;       // zeig das gleiche Bild groß
-    overlay.innerHTML += pictureDescription [i]; // Bild Beschreibung hinzufügen
-    overlay.style.display = "flex";  // zeig das Overlay
+
+  img.onclick = () => {
+    bigImg.src = pictures[i];
+    dialog.classList.add("bigImg"); // img css style
+    dialog.showModal();
+  };
+  container.appendChild(img);
+
+  dialog.addEventListener("click", (e) => {
+    if (e.target === dialog) {
+      dialog.close();
+    }
   });
-
-  container.appendChild(img);                   // In den Container einfügen
 }
-
-closeBtn.addEventListener("click", function() {
-  overlay.style.display = "none";
-  
-});
-
-
 
 // Kontrollausgabe console.log(closeBtn);
